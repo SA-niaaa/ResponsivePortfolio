3416b0bc7832cc822eb64cab6776911ac53b2f72
SA-niaaa <reangsania8516@gmail.com>
2025-07-04 23:13:01 +0530

    ResponsivePortfolioWebsite
    A personal portfolio website built using HTML, CSS, and JavaScript to showcase skills, projects, and contact information. The website features a clean and minimal design with a responsive layout that works seamlessly across devices. It highlights key sections such as About, Skills, Projects, and Contact, providing an organized and user-friendly experience.
    
    This project demonstrates proficiency in front-end web development and responsive design techniques to create a professional online presence.

---
+++ script.js
@@ -0,0 +1,51 @@
+// toggle icon navbar
+// let menuIcon = document.querySelector('#menu');
+// let navbar = document.querySelector('.navbar');
+
+// menuIcon.onclick = () => {
+//     menuIcon.classList.toggle('fa-xmark');
+//     navbar.classList.toggle('active');
+// };
+
+// scroll section active link
+let sections = document.querySelectorAll('section');
+let navLinks = document.querySelectorAll('header nav a');
+
+window.onscroll = () => {
+    let top = window.scrollY;
+
+    sections.forEach(sec => {
+        let offset = sec.offsetTop - 150;
+        let height = sec.offsetHeight;
+        let id = sec.getAttribute('id');
+
+        if (top >= offset && top < offset + height) {
+            navLinks.forEach(link => {
+                link.classList.remove('active');
+            });
+            let activeLink = document.querySelector(`header nav a[href*="${id}"]`);
+            if (activeLink) activeLink.classList.add('active');
+        }
+    });
+
+    // sticky navbar
+    let header = document.querySelector('header');
+    header.classList.toggle('sticky', window.scrollY > 100);
+  menuIcon.classList.remove('fa-xmark');
+  navbar.classList.remove('active');
+};
+
+const icon = document.getElementById('alertIcon');
+  icon.addEventListener('click', () => {
+    alert('Phone Number: +91 9863178567');
+  });
+
+
+ScrollReveal({
+    reset: true, distance: '80px', duration: 2000, delay: 200
+});
+
+
+ScrollReveal().reveal('.h-con, .heading', { origin: 'top'});
+ScrollReveal().reveal('.h-img,.ski-con, .port-box, .contact form', { origin: 'bottom'});
+ScrollReveal().reveal('.h-con h1, .about-img', { origin: 'left'});
